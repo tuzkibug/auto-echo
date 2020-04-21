41add1caf57ac52b37ac0600c15706b55cdf41d6
tuweijian <tuwj@pcl.ac.cn>
2020-04-21 10:30:33 +0800

    4.21

--- controllers/types.go
+++ controllers/types.go
@@ -8,6 +8,19 @@ type MsgMysqlCreate struct {
 	MysqlName  string `json:"mysql_name" form:"mysql_name" query:"mysql_name"`
 }
 
+/*
+header:
+Content-Type:application/json
+body:
+{
+  "username":"pcl",
+  "password":"pcl@123",
+  "domain_name":"default",
+  "tenant_id":"6e57ee69fb0740fc89e53f3bea47a545",
+  "mysql_name":"mymy"
+}
+*/
+
 type MsgMysqlPasswordInitial struct {
 	Newpassword string `json:"newpassword" form:"newpassword" query:"newpassword"`
 }
